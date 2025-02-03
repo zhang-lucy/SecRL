c75ee3559111c205323be118f03ee67ba31806bb
Anand Mudgerikar <amudgeri@purdue.edu>
2025-02-03 09:14:13 -0800

    more exps

--- secgym/run_exp.py
+++ secgym/run_exp.py
@@ -114,9 +114,9 @@ def run_experiment(
 #TODO: fix eval step default value
 def get_args():
     parser = argparse.ArgumentParser(description="Run Experienments")
-    parser.add_argument("--model", "-m", type=str, default="4o-mini", help="Model to use for experiment")
+    parser.add_argument("--model", "-m", type=str, default="gpt-4o", help="Model to use for experiment")
     parser.add_argument("--eval_model", "-e", type=str, default="gpt-4o", help="Model to use for evaluation")
-    parser.add_argument("--cache_seed", type=int, default=74, help="Seed for the cache")
+    parser.add_argument("--cache_seed", type=int, default=75, help="Seed for the cache")
     parser.add_argument("--temperature", type=int, default=0, help="Temperature for the model")
     parser.add_argument("--max_steps", type=int, default=25, help="Maximum number of steps for the agent")
     parser.add_argument("--layer", type=str, default="log", help="Layer to use for the agent")
