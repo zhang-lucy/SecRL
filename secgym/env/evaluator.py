b516e1b892fd3ccf22f5a1a1ccc4740739a82675
Yiran Wu <32823396+kevin666aa@users.noreply.github.com>
2025-02-27 14:35:37 -0500

    update

--- secgym/env/evaluator.py
+++ secgym/env/evaluator.py
@@ -131,7 +131,9 @@ Submitted Answer:
 
 STEP_CHECK_PROMPT = """Given a security question, a submitted answer, and a ground truth solution, please evaluate the correctness of the submitted answer.
 The ground truth solution may contain several steps. Please go through each step of the ground truth solution and evaluate whether the given answer correctly contains key info (the Indicator of Comprise) of that step, which is usually enclosed in `< >`.
-Note that the key information should not be the ones that is already present in the question.
+Note:
+- If the format of the submitted answer is different from the golden answer but the meaning is the same, it should be considered as true.
+- The key information should not be the ones that is already present in the question.
          
 Your response should be in JSON format:
 {
@@ -148,14 +150,16 @@ For each step, you must have two fields:
 """
 
 STEP_CHECK_REFLECTION_PROMPT = """Given a security question, a submitted answer, and a ground truth solution, please evaluate the correctness of the submitted answer.
-The ground truth solution may contain several steps. Please go through each step of the ground truth solution and evaluate whether the given answer correctly contains key info (the Indicator of Comprise) of that step, which is usually enclosed in `< >`. Note that the key information should not be the ones that is already present in the question.
+The ground truth solution may contain several steps. Please go through each step of the ground truth solution and evaluate whether the given answer correctly contains key info (the Indicator of Comprise) of that step, which is usually enclosed in `< >`.
+Note:
+- If the format of the submitted answer is different from the golden answer but the meaning is the same, it should be considered as true.
+- The key information should not be the ones that is already present in the question.
 
 You are also given a previous evaluation of this submitted answer. Reflect on it and serve as a second reviewer to double-check whether the answer is correct.
 
 Your response should be in JSON format:
 {
     "<step_i>" : {
-            "reflection": "<your reflection on previous evaluation>",
             "analysis": "<your analysis>",
             "is_step_correct": "<"True" or "False">,
         },
@@ -163,8 +167,7 @@ Your response should be in JSON format:
 }
 step_i is the step number from the ground truth solution, starting from 0. 
 For each step, you must have three fields:
-- `reflection`: your reflection on the previous evaluation.
-- `analysis`: a quick analysis of whether this step is correct.
+- `analysis`: your reflection on the previous evaluation, and a quick analysis of whether this step is correct.
 - `is_step_correct`: whether the answer matches the key info from this step.
 """
 
